f2d8db13775b126c24dd726e1b451ece2845d587
Eikesales <eikembsales@gmail.com>
2026-08-27 18:13:04 -0300

    Delete ClienteRepository.java

--- ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ativ.ativ.repository;
-
-import com.ativ.ativ.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-    boolean existsByEmail(String email);
-}
